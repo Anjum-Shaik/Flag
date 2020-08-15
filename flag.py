c884abf1152d722ff40eae558898af415c2c7a3b
Anjum Shaik <69392607+Anjum-Shaik@users.noreply.github.com>
2020-08-15 17:53:37 +0530

    Update flag.py

--- flag.py
+++ flag.py
@@ -3,10 +3,10 @@ s=turtle.screen()
 f=turtle.Turtle()
 f.penup()
 f.goto(-180,60)
-#for filling orange color
+#for filling green color
 f.pendown()
 f.begin_fill()
-f.fillcolor("orange")
+f.fillcolor("green")
 f.left(90)
 f.forward(90)
 f.right(90)
@@ -29,10 +29,10 @@ f.forward(400)
 f.left(90)
 f.forward(90)
 
-#for green color
+#for orange color
 
 f.begin_fill()
-f.fillcolor("green")
+f.fillcolor("orange")
 f.forward(90)
 f.left(90)
 f.forward(400)
@@ -49,7 +49,7 @@ f.goto(23,32)
 f.pendown()
 f.begin_fill()
 f.fillcolor("blue")
-f.circle(20)
+f.circle(24)
 f.end_fill()
 #this is must to hold the screen
 turtle.done()
